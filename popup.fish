de2a28040ee9a9e51d24c8d5a091201b8586ad7a
rosvi <rosvi@disroot.org>
2024-07-16 03:06:06 -0300

    Updated README.md with more information

--- popup.fish
+++ popup.fish
@@ -1,19 +1,19 @@
 #! /usr/bin/env fish
 function popup
-	set -x cmd (commandline -co) (commandline -ct)
-	set -S cmd
+		set -x cmd (commandline -co) (commandline -ct)
+		#	set -S cmd
 	kitty \
-			-o background_opacity=1 \
-			-o background=black \
-			#--listen-on unix:/tmp/popup
+		-o background_opacity=1 \
+		-o background=black \
+		#--listen-on unix:/tmp/popup
 		kitten @ launch \
-			--type os-window \
-			--window-title 'Popup' \
-			--copy-env \
-			--cwd \
-			--copy-cmdline \
-			--no-response \
-			/home/vivian/fif.fish $cmd \
+		--type os-window \
+		--window-title 'Popup' \
+		--copy-env \
+		--cwd \
+		--copy-cmdline \
+		--no-response \
+		/home/vivian/fif.fish $cmd \
 		2>/dev/null
 end
 
